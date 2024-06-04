0788a40f772665b30ef9202640e90fbf601a8231
Dhiraj2060 <dhirajsharma2060@gmail.com>
2024-06-04 21:18:33 +0530

    test the 3rd level

--- app/main.py
+++ app/main.py
@@ -10,12 +10,16 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client_socket, _retaddr = server_socket.accept()
     request = client_socket.recv(1024).decode()
+    req = request.split("\r\n")
     url = re.search("GET (.*) HTTP", request).group(1)
     if url == "/":
         client_socket.sendall(OK_RESPONSE)
     elif url.startswith("/echo/"):
         response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(url[6:])}\r\n\r\n{url[6:]}".encode()
         client_socket.sendall(response)
+    elif url.startswith("/user-agent"):
+        user_agent = req[2].split(": ")[1]
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}".encode()    
     else:
         client_socket.sendall(NOTFOUND_RESPONSE)
 if __name__ == "__main__":
